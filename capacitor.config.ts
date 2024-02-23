e6eba4ead4a417f4ea8d85f51ffdfabe4491bf18
Michał Balicki <m.balicki@mente.io>
2024-02-23 11:36:13 +0100

    add name and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'malcy-frontend',
+  appId: 'malcy.game',
+  appName: 'Malcy',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
