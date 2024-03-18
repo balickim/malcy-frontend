457c5c78d0c2b3c152fd67111ca752f534cec495
Michał Balicki <stiekerosiem@gmail.com>
2024-03-18 13:20:00 +0100

    fix

--- src/api/auth/routes.ts
+++ src/api/auth/routes.ts
@@ -1,6 +1,6 @@
 import { fetchWrapper } from "~/api/fetch";
 import { LoginDto } from "~/api/auth/dtos";
 
-export const logIn = async (body: LoginDto): Promise<Response | void> => {
+export const logIn = async (body: LoginDto): Promise<{ access_token: string }> => {
   return fetchWrapper(`${import.meta.env.VITE_API_URL}/auth/login`, { body: JSON.stringify(body), method: 'POST', credentials: 'include' })
 };
