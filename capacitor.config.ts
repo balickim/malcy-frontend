3b8757c9264122a82be832d8dff9baec16ee0bff
Michał Balicki <stiekerosiem@gmail.com>
2024-05-03 17:10:53 +0200

    update logo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'malcy.game',
-  appName: 'Malcy',
-  webDir: 'dist',
+  appId: "malcy.game",
+  appName: "Malcy",
+  webDir: "dist",
   server: {
-    androidScheme: 'http'
+    androidScheme: "http",
   },
   plugins: {
     CapacitorHttp: {
