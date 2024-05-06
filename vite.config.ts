681c1caa098306e94106eef04217eb4727a10f45
Michał Balicki <stiekerosiem@gmail.com>
2024-05-06 08:56:39 +0200

    add why-did-you-render package

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy()],
+  plugins: [
+    react({
+      jsxImportSource: "@welldone-software/why-did-you-render",
+    }),
+    legacy(),
+  ],
   resolve: {
     alias: {
       "~": "/src",
---
+++ wdyr.ts
@@ -0,0 +1,15 @@
+/// <reference types="@welldone-software/why-did-you-render" />
+import * as React from "react";
+
+if (import.meta.env.DEV) {
+  const { default: wdyr } = await import(
+    "@welldone-software/why-did-you-render"
+  );
+
+  wdyr(React, {
+    include: [/.*/],
+    exclude: [/^Ion/, /^Link/, /^Route/, /^Redirect/],
+    trackHooks: true,
+    trackAllPureComponents: true,
+  });
+}
